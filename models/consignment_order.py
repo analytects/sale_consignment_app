d7ce0b9cb6db64b0ed4bf2e4ba8876a7e1322729
jlorenzo-analytects <jonathan.lorenzo@analytects.com>
2024-10-04 17:01:09 -0600

    update method create

--- models/consignment_order.py
+++ models/consignment_order.py
@@ -525,6 +525,7 @@ class ConsignmentOrderLine(models.Model):
         return super(ConsignmentOrderLine, self).create(vals)
     """
 
+    """
     @api.model
     def create(self, vals):
         if vals.get('product_id'):
@@ -545,6 +546,7 @@ class ConsignmentOrderLine(models.Model):
                 vals['product_price'] = product.lst_price
 
         return super(ConsignmentOrderLine, self).create(vals)
+    """
 
 class ConsignmentOrderLot(models.Model):
     _name = "consignment.order.lot"
