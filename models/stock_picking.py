3f7936f4d4ef9930936c86f7903f097081d9f072
jlorenzo-analytects <jonathan.lorenzo@analytects.com>
2024-10-04 10:54:33 -0600

    handle duplicate pickings with the button_validate method

--- models/stock_picking.py
+++ models/stock_picking.py
@@ -7,3 +7,17 @@ class StockPicking(models.Model):
     _inherit = "stock.picking"
 
     is_consignment = fields.Boolean(string='Is Consignment')
+
+    def button_validate(self):
+        res = super(StockPicking, self).button_validate()
+
+        stock_picking_to_delete = self.env['stock.picking'].search([
+            ('origin', 'like', 'SC-'),
+            ('state', '=', 'assigned'),
+            ('origin', '=', self.origin)
+        ], limit=1)
+
+        if stock_picking_to_delete:
+            stock_picking_to_delete.unlink()
+
+        return res
