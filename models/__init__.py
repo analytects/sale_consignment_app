7deac67696e5f422eec829f24eba79f32d1fb283
jlorenzo-analytects <jonathan.lorenzo@analytects.com>
2024-10-16 12:42:56 -0600

    add changes for transfers

--- models/__init__.py
+++ models/__init__.py
@@ -4,3 +4,4 @@ from . import stock_warehouse
 from . import consignment_order
 from . import sale_order
 from . import stock_picking
+from . import stock_return_picking
---
+++ models/stock_return_picking.py
@@ -0,0 +1,23 @@
+from odoo import api, models, fields, _
+from odoo.exceptions import UserError
+
+class StockReturnPicking(models.TransientModel):
+    _inherit = 'stock.return.picking'
+
+    @api.onchange('product_return_moves')
+    def _onchange_product_return_moves(self):
+        picking_id = self.env.context.get('active_id')
+        if picking_id:
+            picking = self.env['stock.picking'].browse(picking_id)
+            if picking.origin and 'SC-' in picking.origin:
+                
+                consignment_orders = self.env['consignment.order'].search([('name', '=', picking.origin)])
+                
+                if not consignment_orders:
+                    raise UserError(_("No se encontraron pedidos de consignación que coincidan con el origen del picking."))
+
+                for consignment_order in consignment_orders:
+                    for consignment_line in consignment_order.line_ids:
+                        for return_move in self.product_return_moves:
+                            if return_move.product_id == consignment_line.product_id:
+                                return_move.quantity = consignment_line.remain_qty
