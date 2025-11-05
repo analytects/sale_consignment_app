f37d452ec5e9c5b859b391020e033c2fd932fc99
Kirby <kairi.franco@analytects.com>
2025-11-05 16:24:18 -0600

    added default fields

---
+++ models/res_users.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from odoo import api, fields, models, _
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    default_consignment_warehouse_id = fields.Many2one(
+        comodel_name='stock.warehouse',
+        string='Almacén de Consignación'
+    )
+    default_route_id = fields.Many2one(
+        comodel_name='stock.route',
+        string='Ruta de Consignación'
+    )
